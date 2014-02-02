82fc4a7625b313944ca0567eeb80d6e91bfbbbbb
Zoffix Znet <cpan@zoffix.com>
2014-02-02 15:42:48 -0500

    Initial setup

---
+++ examples/json-ld.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use lib qw{../lib  lib};
+
+use JSON::LD;
---
+++ lib/JSON/LD.pm
@@ -0,0 +1,45 @@
+package JSON::LD;
+
+use strict;
+use warnings;
+
+# VERSION
+
+q|
+All programmers are playwrights and all computers are lousy actors.
+|;
+
+__END__
+
+=encoding utf8
+
+=head1 EXAMPLES
+
+This distribution contains C<examples/> directory, with usage examples
+of each plugin.
+
+=head1 REPOSITORY
+
+Fork this module on GitHub:
+L<https://github.com/zoffixznet/JSON-LD>
+
+=head1 BUGS
+
+To report bugs or request features, please use
+L<https://github.com/zoffixznet/JSON-LD/issues>
+
+If you can't access GitHub, you can email your request
+to C<bug-JSON-LD at rt.cpan.org>
+
+=head1 AUTHOR
+
+Zoffix Znet <zoffix at cpan.org>
+(L<http://zoffix.com/>, L<http://haslayout.net/>)
+
+=head1 LICENSE
+
+You can use and distribute this module under the same terms as Perl itself.
+See the C<LICENSE> file included in this distribution for complete
+details.
+
+=cut
